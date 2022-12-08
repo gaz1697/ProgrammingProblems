2f7b5123e85544916ab745d12990d696e9742a42
gaz1697 <gazlan111@gmail.com>
2022-12-08 09:56:26 +0300

    thermostat comments

--- stlsolutions/thermostat.cpp
+++ stlsolutions/thermostat.cpp
@@ -5,65 +5,86 @@ signed main(int argc, char const *argv[])
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
+    // Read in the number of test cases
     int t;
     cin >> t;
+
+    // For each test case
     while (t--)
     {
+        // Read in the values of l, r, x, oldTemp, and newTemp
         long long l, r, x, oldTemp, newTemp;
         cin >> l >> r >> x >> oldTemp >> newTemp;
-        long long res;
+
+        // Set the result to -1 by default
+        long long res = -1;
+
+        // If the old and new temperatures are the same, the result is 0
         if (oldTemp == newTemp)
         {
             res = 0;
         }
+        // Otherwise, calculate the difference between the old and new temperatures
         else
         {
             long long diff = abs(oldTemp - newTemp);
+
+            // If the difference is greater than or equal to x, the result is 1
             if (diff >= x)
             {
                 res = 1;
             }
+            // Otherwise, the result is -1 by default
             else
             {
-                res = -1;
+                // Calculate the differences between the old temperature and the upper and lower bounds,
+                // and the differences between the new temperature and the upper and lower bounds
                 long long upperDiff = abs(oldTemp - r);
                 long long lowerDiff = abs(oldTemp - l);
                 long long upper = abs(r - newTemp);
                 long long lower = abs(l - newTemp);
+
+                // If difference between the new temperature and the upper bound is greater than or equal to x
                 if (upper >= x)
                 {
+                    // If the difference between the old temperature and upper bounds is greater than or equal to x,
+                    // then the result is 2
                     if (upperDiff >= x)
                     {
                         res = 2;
                     }
-                    else
+                    // Otherwise, if the difference between the old temperature and lower bounds is greater than or equal to x,
+                    // then the result is 3
+                    else if (lowerDiff >= x)
                     {
-                        if (lower >= x)
-                        {
-                            res = 3;
-                        }
+                        res = 3;
                     }
                 }
+
+                // If the difference between the new temperature and lower bound is greater than or equal to x
                 if (lower >= x)
                 {
+                    // If the difference between the old temperature and lower bounds is greater than or equal to x,
+                    // then the result is 2
                     if (lowerDiff >= x)
                     {
                         res = 2;
                     }
-                    else
+                    // Otherwise, if the difference between the old temperature and upper bounds is greater than or equal to x
+                    else if (upperDiff >= x)
                     {
-                        if (upper >= x)
+                        // If the result is not already 2, then set it to 3
+                        if (res != 2)
                         {
-                            if (res != 2)
-                            {
-                                res = 3;
-                            }
+                            res = 3;
                         }
                     }
                 }
             }
         }
 
-        cout << res << '/n';
+        // Print the result
+        cout << res << '\n';
     }
 }
